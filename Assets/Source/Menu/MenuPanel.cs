6a85b3af35c862362b856dcdfe501acf612a16c0
RudeGalaxy1010 <Karev007@bk.ru>
2023-07-27 17:24:17 +0300

    Settings + saves

--- Assets/Source/Menu/MenuPanel.cs
+++ Assets/Source/Menu/MenuPanel.cs
@@ -6,17 +6,22 @@ public class MenuPanel : MonoBehaviour
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _settingsButton;
     [SerializeField] private SettingsPanel _settingsPanel;
+    [SerializeField] private DifficultySettings _difficultySettings;
+
+    private Difficulty _difficulty;
 
     private void OnEnable()
     {
         _playButton.onClick.AddListener(OnPlayButtonClicked);
         _settingsButton.onClick.AddListener(OnSettingsButtonClicked);
+        _difficultySettings.DifficultyChanged += OnDifficultyChanged;
     }
 
     private void OnDisable()
     {
         _playButton.onClick.RemoveListener(OnPlayButtonClicked);
         _settingsButton.onClick.RemoveListener(OnSettingsButtonClicked);
+        _difficultySettings.DifficultyChanged -= OnDifficultyChanged;
     }
 
     private void OnPlayButtonClicked()
@@ -27,5 +32,10 @@ public class MenuPanel : MonoBehaviour
     private void OnSettingsButtonClicked()
     {
         _settingsPanel.gameObject.SetActive(true);
+    }
+
+    private void OnDifficultyChanged(Difficulty difficulty)
+    {
+        _difficulty = difficulty;
     }
 }
